0e5ca6a8b7fee90a3ed6e8daac7754b5e5d99b24
Pushpendra <pushpendrasahu1122@gmail.com>
2025-03-25 23:40:01 +0530

    backend intiated

---
+++ src/main/java/com/ps/MeetchatApplication.java
@@ -0,0 +1,13 @@
+package com.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ps/meetchat/MeetchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.meetchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
